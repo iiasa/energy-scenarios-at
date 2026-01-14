87b73ae4f8e906c88931ee92ac9957a984a67ff9
Daniel Huppmann <dh@dergelbesalon.at>
2026-01-14 09:31:05 +0100

    Remove region-processing from the workflow function

--- workflow.py
+++ workflow.py
@@ -9,7 +9,6 @@ here = Path(__file__).absolute().parent
 def main(df: pyam.IamDataFrame) -> pyam.IamDataFrame:
     """Project/instance-specific workflow for scenario processing"""
 
-    # Run the validation and region-processing
-    dsd = DataStructureDefinition(here / "definitions")
-    processor = RegionProcessor.from_directory(path=here / "mappings", dsd=dsd)
-    return process(df, dsd, processor=processor)
+    # Run the validation
+    definition = DataStructureDefinition(here / "definitions")
+    return process(df, definition)
